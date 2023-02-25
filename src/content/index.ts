645d255a726cab5d3428ec7926bd603a5c8c3446
muertet <fp557s@gmail.com>
2023-02-25 15:10:10 +0100

    + Trigger xupopter when needed

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,22 +1,45 @@
 import { shadowDOM } from "src/stores/shadow";
+import { shouldLoadOn } from "src/storage";
 import Overlay from "../components/Overlay.svelte";
 
-// Some svelte component on the page
-const wrapper = document.createElement('div');
-wrapper.id = "xupopter-extension-container";
+const showXupopter = () => {
+    // Some svelte component on the page
+    const wrapper = document.createElement('div');
+    wrapper.id = "xupopter-extension-container";
 
-wrapper.style.all = 'initial';
-/*
-wrapper.style.position = "absolute";
-wrapper.style.top = "0";
-wrapper.style.right = "0";
-wrapper.style.zIndex = "2147483647";
-wrapper.style.height = "100vh";
-*/
+    wrapper.style.all = 'initial';
+    /*
+    wrapper.style.position = "absolute";
+    wrapper.style.top = "0";
+    wrapper.style.right = "0";
+    wrapper.style.zIndex = "2147483647";
+    wrapper.style.height = "100vh";
+    */
 
-document.body.append(wrapper);
-const shadow = wrapper.attachShadow({mode: "open"});
+    document.body.append(wrapper);
+    const shadow = wrapper.attachShadow({mode: "open"});
 
-shadowDOM.set(shadow);
+    shadowDOM.set(shadow);
 
-new Overlay({ target: shadow });
+    new Overlay({ target: shadow });
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (typeof request.showXupopter === "undefined") {
+        return;
+    }
+    const container = document.getElementById("xupopter-extension-container");
+    if (request.showXupopter && !container) {
+        showXupopter();
+    } else if (!request.showXupopter && container) {
+        container.remove();
+    }
+});
+
+shouldLoadOn(window.location.hostname).then(showLoad => {
+    if (!showLoad) {
+        return;
+    }
+
+    showXupopter();
+});
--- src/storage.ts
+++ src/storage.ts
@@ -1,13 +1,21 @@
 type IStorage = {
     xupopterClient?: string;
+    visibility?: string[];
 };
 
 const defaultStorage: IStorage = {
     xupopterClient: null,
+    visibility: []
 };
 
 export const storage = {
     get: (): Promise<IStorage> =>
         chrome.storage.sync.get(defaultStorage) as Promise<IStorage>,
     set: (value: IStorage): Promise<void> => chrome.storage.sync.set(value)
+};
+
+
+export const shouldLoadOn = async (host: string) : Promise<boolean> => {
+    const data = await storage.get();
+    return data.visibility.includes(host);
 };
